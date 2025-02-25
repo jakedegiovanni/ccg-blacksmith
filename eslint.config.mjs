d46f9c610420e861035dbe34734436ba7bf4e6aa
Jake Degiovanni <degiojake@gmail.com>
2025-02-25 19:55:08 +0000

    skeleton workspace project setup containg a rules engine library and a web client

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
